55628c597e8e2fe239583a0be8fede2b5e6758d4
Mevan Samaratunga <mevansam@gmail.com>
2021-04-19 23:51:21 -0400

    export test dispatcher fn and published 0.0.6

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 import ActionTester, { testActionDispatcher } from './redux/action-tester'; 
 import StateTester from './redux/state-tester';
-export { ActionTester, StateTester };
+export { ActionTester, testActionDispatcher, StateTester };
